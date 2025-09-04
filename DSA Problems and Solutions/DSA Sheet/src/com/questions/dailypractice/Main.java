c5462b44c2923f4ea8eb77118b4f3e6124ef75b4
unknown <ronakpanchal1998@gmail.com>
2025-09-04 20:23:55 +0530

    recursion practice

--- DSA Problems and Solutions/DSA Sheet/src/com/questions/dailypractice/Main.java
+++ DSA Problems and Solutions/DSA Sheet/src/com/questions/dailypractice/Main.java
@@ -6,21 +6,26 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-        int arr[] = {2,3};
-        int target = 2;
-        List<Integer> list = new ArrayList<>();
-        test(0,arr,list,0);
+        int arr[] = {1,2,3};
+        List<List<Integer>> list = new ArrayList<>();
+        test(arr,list,new ArrayList<>(),new boolean[arr.length]);
         System.out.println(list);
     }
 
-    public static void test(int index,int arr[], List<Integer>temp, int sum){
-        if(index == arr.length){
-            temp.add(sum);
+    public static void test(int arr[], List<List<Integer>> list,List<Integer>temp,boolean[] freq){
+        if(temp.size() == arr.length){
+            list.add(new ArrayList<>(temp));
             return;
         }
-        test(index+1,arr,temp,sum+arr[index]);
-        test(index+1,arr,temp,sum);
-
+        for(int i=0;i<arr.length;i++){
+            if(!freq[i]){
+                freq[i] = true;
+                temp.add(arr[i]);
+                test(arr,list,temp,freq);
+                temp.remove(temp.size()-1);
+                freq[i] = false;
+            }
+        }
     }
 
 }
