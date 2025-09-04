7b3989c8a1a31e487c39fcc677db37b6ce402de5
unknown <ronakpanchal1998@gmail.com>
2025-09-04 19:22:04 +0530

    recursion practicce

--- DSA Problems and Solutions/DSA Sheet/src/com/questions/dailypractice/Main.java
+++ DSA Problems and Solutions/DSA Sheet/src/com/questions/dailypractice/Main.java
@@ -1,45 +1,26 @@
 package com.questions.dailypractice;
 
-/*
-Problem Statement: Maximum Points You Can Obtain from Cards
+import java.util.ArrayList;
+import java.util.List;
 
-There are several cards arranged in a row, and each card has an associated number of points.
-You can take exactly k cards from either the beginning or the end of the array.
-Return the maximum score you can obtain.
-
-Examples:
-1. Input: cardPoints = [1,2,3,4,5,6,1], k = 3 → Output: 12
-   Explanation: Best choice is taking [6,5,1] → sum = 12.
-
-2. Input: cardPoints = [2,2,2], k = 2 → Output: 4
-   Explanation: Any two cards = 2 + 2 = 4.
-
-3. Input: cardPoints = [9,7,7,9,7,7,9], k = 7 → Output: 55
-   Explanation: Must take all cards = total sum.
-*/
 public class Main {
 
     public static void main(String[] args) {
-        int arr[] = {6,2,3,4,7,2,1,7,1};
-        int k = 4;
-        System.out.println(test(arr,k));
+        int arr[] = {2,3};
+        int target = 2;
+        List<Integer> list = new ArrayList<>();
+        test(0,arr,list,0);
+        System.out.println(list);
     }
 
-    public static int test(int arr[] ,int k){
-        int sum=0,maxSum=0,n=arr.length;
-        for(int i=0;i<k;i++){
-            sum = sum + arr[i];
+    public static void test(int index,int arr[], List<Integer>temp, int sum){
+        if(index == arr.length){
+            temp.add(sum);
+            return;
         }
-        maxSum = sum;
-        int leftIndex = k-1;
-        int rightIndex = n-1;
-        for(int i=0;i<k;i++){
-            sum -= arr[leftIndex];
-            sum += arr[rightIndex];
-            maxSum = Math.max(maxSum,sum);
-            leftIndex--;
-            rightIndex--;
-        }
-        return maxSum;
+        test(index+1,arr,temp,sum+arr[index]);
+        test(index+1,arr,temp,sum);
+
     }
+
 }
