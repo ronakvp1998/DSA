c9f7e2d3027e3369951fb1e75fc44e5ef9405ffe
ronak <ronakpanchal1998@gmail.com>
2026-01-10 11:22:56 +0530

    update the graphq problems

--- DSA Problems and Solutions/DSA Sheet/src/com/questions/practice/graph/GraphTest.java
+++ DSA Problems and Solutions/DSA Sheet/src/com/questions/practice/graph/GraphTest.java
@@ -7,49 +7,48 @@ import java.util.PriorityQueue;
 public class GraphTest{
 
     static class Pair{
-        int effort;
         int row;
         int col;
-        Pair(int effort,int row,int col){
+        int effort;
+        Pair(int row,int col,int effort){
+            this.row = row;this.col = col;
             this.effort = effort;
-            this.row = row;
-            this.col = col;
         }
     }
-    private static int MinimumEffort(int[][] heights) {
+
+    private static int minEffort(int [][] heights){
         int n = heights.length;
         int m = heights[0].length;
         int dist[][] = new int[n][m];
-        for(int[] it : dist){
-            Arrays.fill(it,Integer.MAX_VALUE);
+        for(int [] it : dist){
+            Arrays.fill(it,(int)1e9);
         }
-        dist[0][0] = 0;
         PriorityQueue<Pair> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a.effort));
         pq.add(new Pair(0,0,0));
-        int delRow[] = {-1,0,1,0};
-        int delCol[] = {0,1,0,-1};
+        dist[0][0] = 0;
+        int []delRow = {-1,0,1,0};
+        int []delCol = {0,1,0,-1};
         while (!pq.isEmpty()){
             Pair node = pq.poll();
-            int effort = node.effort;
             int row = node.row;
-            int col = node.col;
-            if(row == n-1 && col == m-1){
+            int col = node.col;;
+            int effort = node.effort;
+            // Destination reached
+            if (row == n - 1 && col == m - 1)
                 return effort;
-            }
             for(int i=0;i<4;i++){
                 int r = row + delRow[i];
                 int c = col + delCol[i];
-                if(r >= 0 && r < n && c >= 0 && c < m){
+                if(r>=0 && r<n && c>=0 && c<m){
                     int newEffort = Math.max(effort,Math.abs(heights[row][col] - heights[r][c]));
                     if(newEffort < dist[r][c]){
                         dist[r][c] = newEffort;
-                        pq.add(new Pair(newEffort,r,c));
+                        pq.add(new Pair(r,c,newEffort));
                     }
                 }
             }
         }
         return 0;
     }
-
 
 }
